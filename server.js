9b1b7e501e034196bbdf4d2ce9a4e2768119faaf
Mtete <mtadf13@gmail.com>
2019-03-16 17:00:15 +0000

    third commit

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/dis/tour-of-heroes-aubilokos'));
+app.use(express.static(__dirname + '/dist/tour-of-heroes-aubilokos'));
 
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/dis/tour-of-heroes-aubilokos/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/tour-of-heroes-aubilokos/index.html'));
 })
 
 app.listen(process.env.PORT || 8080);
